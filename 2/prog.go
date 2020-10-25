14a00378cfdd4cd51810cee275c45090c371b64d
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2020-10-25 22:56:45 +0000

    feat: day 2, part 2 - done

--- 2/prog.go
+++ 2/prog.go
@@ -7,43 +7,71 @@ import (
 )
 
 func main() {
+	part1()
+	part2()
+}
+
+func part2() {
 	data := utils.LoadInput()
 	ints := utils.StringsToInts(data)
 	f.Printf("input: %v\n", ints)
-	ints[1] = 12
-	ints[2] = 2
-	output := Run(ints)
-	f.Printf("output: %v\n", output)
+	for noun := 0; noun < 100; noun++ {
+		for verb := 0; verb < 100; verb++ {
+			ints[1] = int64(noun)
+			ints[2] = int64(verb)
+			output := IntCodeComputer(ints)
+			if output[0] == 19690720 {
+				f.Printf("noun: %v, verb: %v\n", noun, verb)
+				f.Println("Answer to part 2:", 100*noun+verb)
+				break
+			}
+		}
+	}
 }
 
-// Run ...
-func Run(input []int64) []int64 {
+func part1() {
+	f.Println("Running part 1...")
+	data := utils.LoadInput()
+	ints := utils.StringsToInts(data)
+	// f.Printf("input: %v\n", ints)
+	ints[1] = 12 // noun
+	ints[2] = 2  // verb
+	output := IntCodeComputer(ints)
+	// f.Printf("input: %v\n", ints)
+	// f.Printf("output: %v\n", output)
+	f.Println("Answer to part 1:", output[0])
+}
+
+// IntCodeComputer ...
+func IntCodeComputer(input []int64) []int64 {
 	data := make([]int64, len(input))
 	copy(data, input)
 
 	// f.Println(data)
-	pos := 0
-	opcode := data[pos]
-	for opcode != 99 && pos < len(data) {
+	address := 0
+	opcode := data[address]
+	for opcode != 99 && address < len(data) {
 		// f.Println(data)
 		switch opcode {
 		case 1:
 			// f.Println("opcode is 1")
-			n1 := data[data[pos+1]]
-			n2 := data[data[pos+2]]
-			data[data[pos+3]] = n1 + n2
+			n1 := data[data[address+1]]
+			n2 := data[data[address+2]]
+			data[data[address+3]] = n1 + n2
+			address += 4
 		case 2:
 			// f.Println("opcode is 2")
-			n1 := data[data[pos+1]]
-			n2 := data[data[pos+2]]
-			data[data[pos+3]] = n1 * n2
+			n1 := data[data[address+1]]
+			n2 := data[data[address+2]]
+			data[data[address+3]] = n1 * n2
+			address += 4
 		case 99:
 			// f.Println("opcode is 99")
 			opcode = 99
+			address++
 			break
 		}
-		pos += 4
-		opcode = data[pos]
+		opcode = data[address]
 	}
 	// f.Println(data)
 	return data
