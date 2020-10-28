8ba284edc2118a9f7af2b9b4453f18229902dced
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2020-10-28 16:50:38 +0000

    feat: day 4, part 2 - done

--- 4/prog.go
+++ 4/prog.go
@@ -6,25 +6,46 @@ import (
 	"strings"
 )
 
+// 236491-713787 <- range of password
 func main() {
-	// 236491-713787 <- range of password
-	counter := 0
+	part1PasswordCount := 0
+	part2PasswordCount := 0
 	for i := 236491; i < 713788; i++ {
-		split := strings.Split(strconv.Itoa(i), "")
-		neverDecreasing := false
+		digits := strings.Split(strconv.Itoa(i), "")
+		digitsDecrease := false
 		adjacentDigits := false
-		for j := 1; j < len(split); j++ {
-			if adjacentDigits || split[j-1] == split[j] {
+
+		digitPositions := map[string][]int{}
+		digitPositions[digits[0]] = []int{0}
+
+		for j := 1; j < len(digits); j++ {
+			pos, prs := digitPositions[digits[j]]
+			if prs {
+				digitPositions[digits[j]] = append(pos, j)
+			} else {
+				digitPositions[digits[j]] = []int{j}
+			}
+			if adjacentDigits || digits[j-1] == digits[j] {
 				adjacentDigits = true
 			}
-			if split[j-1] > split[j] {
-				neverDecreasing = true
+			if digits[j-1] > digits[j] {
+				digitsDecrease = true
 				break
 			}
 		}
-		if !neverDecreasing && adjacentDigits {
-			counter++
+		viable := false
+		if !digitsDecrease {
+			for _, v := range digitPositions {
+				if !viable && len(v) == 2 && v[0]+1 == v[1] {
+					viable = true
+					part2PasswordCount++
+				}
+			}
+		}
+		if !digitsDecrease && adjacentDigits {
+			part1PasswordCount++
 		}
 	}
-	fmt.Println("number of passwords:", counter)
+	fmt.Println("number of passwords:", part1PasswordCount)
+	fmt.Println("number of new passwords:", part2PasswordCount)
 }
