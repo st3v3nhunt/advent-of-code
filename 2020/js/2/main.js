fff0f34e28e02d54ac246205198e7b27218e47b5
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2020-12-02 07:12:50 +0000

    feat(day2): js - part 2

--- 2020/js/2/main.js
+++ 2020/js/2/main.js
@@ -4,28 +4,47 @@ async function getInput () {
   return getDayInputAsLines(2)
 }
 
-async function answers () {
+async function partOne () {
   const input = await getInput()
   let count = 0
 
   for (const line of input) {
     const data = line.split(':')
-    // console.log(data)
     const [pattern, password] = [data[0], data[1]]
-    console.log(pattern, password)
     const req = pattern.split(' ')
     const [reqOccurs, reqChar] = [req[0], req[1]]
     const occurs = password.split(reqChar).length - 1
     const [min, max] = reqOccurs.split('-')
-    console.log(pattern, password, reqOccurs, reqChar, min, max, occurs)
+    // console.log(pattern, password, reqOccurs, reqChar, min, max, occurs)
+
     if (occurs >= min && occurs <= max) {
-      // requirements met
       count++
     }
   }
-  console.log(count)
+  console.log('part 1 answer:', count)
+}
+
+async function partTwo () {
+  const input = await getInput()
+  let count = 0
+
+  for (const line of input) {
+    const data = line.split(':')
+    const [pattern, password] = [data[0], data[1]]
+    const req = pattern.split(' ')
+    const [reqPositions, reqChar] = [req[0], req[1]]
+    const [posOne, posTwo] = reqPositions.split('-')
+    // console.log(pattern, password, reqPositions, reqChar, posOne, posTwo)
+
+    // if (password[posOne] === reqChar ^ password[posTwo] === reqChar) {
+    if (((password[posOne] === reqChar) !== (password[posTwo] === reqChar))) {
+      count++
+    }
+  }
+  console.log('part 2 answer:', count)
 }
 
 (async function run () {
-  await answers()
+  await partOne()
+  await partTwo()
 }())
