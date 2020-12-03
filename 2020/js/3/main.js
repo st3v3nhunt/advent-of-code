17d9499b768a6b6adbb18782088397f9ddeffcf0
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2020-12-03 21:56:58 +0000

    refactor(day2): use modulo for efficiency

--- 2020/js/3/main.js
+++ 2020/js/3/main.js
@@ -7,27 +7,20 @@ async function getInput () {
 async function partOne () {
   const input = await getInput()
 
-  const _31 = traverseForest(createForest(input, 3), 3, 1)
+  const _31 = traverseForest(input, 3, 1)
 
-  console.log('part 1 answer:', _31) // 282
+  console.log('part 1 answer:', _31)
 }
 
-function createForest (input, xMove) {
-  const currentLength = input.length
-  const currentWidth = input[0].length
-  const requiredWidth = Math.ceil(currentLength / currentWidth) * xMove
-
-  return input.map(line => line.repeat(requiredWidth))
-}
-
-// returns the number of trees ('#') encountered whilst traversing the given
-// forest with the given x, y movement
-function traverseForest (forest, xMove, yMove) {
+// returns the number of trees ('#') encountered whilst traversing
+// the given input with the given x, y movement
+function traverseForest (input, xMove, yMove) {
   let x = 0
   let y = 0
   let treeCount = 0
-  while (y < forest.length) {
-    if (forest[y][x] === '#') {
+  const rowWidth = input[0].length
+  while (y < input.length) {
+    if (input[y][x % rowWidth] === '#') {
       treeCount++
     }
     x += xMove
@@ -39,16 +32,16 @@ function traverseForest (forest, xMove, yMove) {
 async function partTwo () {
   const input = await getInput()
 
-  const _11 = traverseForest(createForest(input, 1), 1, 1)
-  const _31 = traverseForest(createForest(input, 3), 3, 1)
-  const _51 = traverseForest(createForest(input, 5), 5, 1)
-  const _71 = traverseForest(createForest(input, 7), 7, 1)
-  const _12 = traverseForest(createForest(input, 1), 1, 2)
+  const _11 = traverseForest(input, 1, 1)
+  const _31 = traverseForest(input, 3, 1)
+  const _51 = traverseForest(input, 5, 1)
+  const _71 = traverseForest(input, 7, 1)
+  const _12 = traverseForest(input, 1, 2)
 
-  console.log('part 2 answer:', _11 * _31 * _51 * _71 * _12) // 958815792
+  console.log('part 2 answer:', _11 * _31 * _51 * _71 * _12)
 }
 
 (async function run () {
-  await partOne()
-  await partTwo()
+  await partOne() // 282
+  await partTwo() // 958815792
 }())
