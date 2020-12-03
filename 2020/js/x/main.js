86cba40f6c7f05f4ba0db93b6ac17987e4ef7e3f
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2020-12-03 22:01:29 +0000

    feat: add basic template for 2020 JS

---
+++ 2020/js/x/main.js
@@ -0,0 +1,20 @@
+const { getDayInputAsLines } = require('../lib/utils')
+
+async function getInput () {
+  return getDayInputAsLines(0)
+}
+
+async function partOne () {
+  const input = await getInput()
+  console.log('part 1 answer:', 'answer')
+}
+
+async function partTwo () {
+  const input = await getInput()
+  console.log('part 2 answer:', 'answer')
+}
+
+(async function run () {
+  await partOne()
+  await partTwo()
+}())
