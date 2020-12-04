528aeb283022b228ee6e09e90c9c309916105f4b
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2020-12-04 18:55:25 +0000

    feat(day3|rust): part 2 complete

--- 2020/rust/three/src/main.rs
+++ 2020/rust/three/src/main.rs
@@ -7,26 +7,38 @@ fn main() {
 
     let ans_two = part_two(file_contents);
     println!("Part two answer: {}", ans_two);
-    // assert_eq!(ans_two, 958815792);
+    assert_eq!(ans_two, 958815792);
 }
 
-fn part_one(file_contents: &str) -> u32 {
+fn traverse(file_contents: &str, move_x: usize, move_y: usize) -> u32 {
     let (mut x, mut y) = (0, 0);
-    let (move_x, move_y) = (3, 1);
     let width = file_contents.lines().nth(0).unwrap().len();
     let mut tree_count = 0;
+    let mut line_counter = 0;
 
     file_contents.lines().for_each(|line| {
-        if line.chars().nth(x % width).unwrap() == '#' {
-            tree_count = tree_count + 1
+        if (line_counter % move_y) == 0 {
+            if line.chars().nth(x % width).unwrap() == '#' {
+                tree_count = tree_count + 1;
+            }
+            x = x + move_x;
+            y = y + move_y;
         }
-        x = x + move_x;
-        y = y + move_y;
+        line_counter = line_counter + 1;
     });
 
     tree_count
 }
 
+fn part_one(file_contents: &str) -> u32 {
+    traverse(file_contents, 3, 1)
+}
+
 fn part_two(file_contents: &str) -> u32 {
-    file_contents.len() as u32
+    let _11 = traverse(file_contents, 1, 1);
+    let _31 = traverse(file_contents, 3, 1);
+    let _51 = traverse(file_contents, 5, 1);
+    let _71 = traverse(file_contents, 7, 1);
+    let _12 = traverse(file_contents, 1, 2);
+    _11 * _31 * _51 * _71 * _12
 }
