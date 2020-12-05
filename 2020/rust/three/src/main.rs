9d21e0261352b0c01d95765a4eb7264865c0d528
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2020-12-05 00:18:39 +0000

    refactor(day4|rust): clippy suggestions

--- 2020/rust/three/src/main.rs
+++ 2020/rust/three/src/main.rs
@@ -12,19 +12,19 @@ fn main() {
 
 fn traverse(file_contents: &str, move_x: usize, move_y: usize) -> u32 {
     let (mut x, mut y) = (0, 0);
-    let width = file_contents.lines().nth(0).unwrap().len();
+    let width = file_contents.lines().next().unwrap().len();
     let mut tree_count = 0;
     let mut line_counter = 0;
 
     file_contents.lines().for_each(|line| {
         if (line_counter % move_y) == 0 {
             if line.chars().nth(x % width).unwrap() == '#' {
-                tree_count = tree_count + 1;
+                tree_count += 1;
             }
-            x = x + move_x;
-            y = y + move_y;
+            x += move_x;
+            y += move_y;
         }
-        line_counter = line_counter + 1;
+        line_counter += 1;
     });
 
     tree_count
@@ -35,10 +35,10 @@ fn part_one(file_contents: &str) -> u32 {
 }
 
 fn part_two(file_contents: &str) -> u32 {
-    let _11 = traverse(file_contents, 1, 1);
-    let _31 = traverse(file_contents, 3, 1);
-    let _51 = traverse(file_contents, 5, 1);
-    let _71 = traverse(file_contents, 7, 1);
-    let _12 = traverse(file_contents, 1, 2);
-    _11 * _31 * _51 * _71 * _12
+    let move_1_1 = traverse(file_contents, 1, 1);
+    let move_3_1 = traverse(file_contents, 3, 1);
+    let move_5_1 = traverse(file_contents, 5, 1);
+    let move_7_1 = traverse(file_contents, 7, 1);
+    let move_1_2 = traverse(file_contents, 1, 2);
+    move_1_1 * move_3_1 * move_5_1 * move_7_1 * move_1_2
 }
