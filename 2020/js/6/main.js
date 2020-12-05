d2256e441a98bd6041d341324c3e09ea052384bc
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2020-12-05 07:59:39 +0000

    chore(day5|js): setup

---
+++ 2020/js/6/main.js
@@ -0,0 +1,23 @@
+const assert = require('assert')
+const { getDayInputAsLines } = require('../lib/utils')
+
+async function getInput () {
+  return getDayInputAsLines(6)
+}
+
+async function partOne () {
+  const input = await getInput()
+  console.log('part 1 answer:', input.length)
+  assert.equal(input.length, 0)
+}
+
+async function partTwo () {
+  const input = await getInput()
+  console.log('part 2 answer:', input.length)
+  assert.equal(input.length, 0)
+}
+
+(async function run () {
+  await partOne()
+  await partTwo()
+}())
