b040265998b4f1a41620c0a5ef6c7f17506c852f
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2020-12-05 18:17:14 +0000

    feat(day5|rust): part 1 and 2 complete

--- 2020/rust/five/src/main.rs
+++ 2020/rust/five/src/main.rs
@@ -3,17 +3,56 @@ fn main() {
 
     let ans_one = part_one(file_contents);
     println!("Part one answer: {}", ans_one);
-    assert_eq!(ans_one, 0);
+    assert_eq!(ans_one, 828);
 
     let ans_two = part_two(file_contents);
     println!("Part two answer: {}", ans_two);
-    assert_eq!(ans_two, 0);
+    assert_eq!(ans_two, 565);
+}
+
+fn boarding_passes(file_contents: &str) -> Vec<u32> {
+    file_contents
+        .lines()
+        .map(|pass| {
+            let (raw_row, raw_seat) = pass.split_at(7);
+            let row = raw_row
+                .chars()
+                .map(|c| if c == 'B' { '1' } else { '0' })
+                .collect::<String>();
+            let row = u32::from_str_radix(row.as_str(), 2).unwrap();
+
+            let seat = raw_seat
+                .chars()
+                .map(|c| if c == 'R' { '1' } else { '0' })
+                .collect::<String>();
+            let seat = u32::from_str_radix(seat.as_str(), 2).unwrap();
+            row * 8 + seat
+        })
+        .collect::<_>()
 }
 
 fn part_one(file_contents: &str) -> u32 {
-    file_contents.len() as u32
+    boarding_passes(file_contents)
+        .iter()
+        .max()
+        .unwrap()
+        .to_owned()
 }
 
 fn part_two(file_contents: &str) -> u32 {
-    file_contents.len() as u32
+    let mut passes = boarding_passes(file_contents);
+    passes.sort();
+    let mut seat_id = 0;
+    passes.iter().fold(0_u32, |prev, cur| {
+        if prev == 0 {
+            *cur
+        } else if *cur == prev + 1 {
+            *cur
+        } else {
+            seat_id = prev + 1;
+            println!("seat_id: {}", seat_id);
+            *cur
+        }
+    });
+    seat_id
 }
