f0216110f399e779ea4ad9971219cba7626a5499
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2020-12-07 21:47:46 +0000

    feat(day7|js): part 1 complete

--- 2020/js/7/main.js
+++ 2020/js/7/main.js
@@ -5,8 +5,49 @@ async function getInput () {
   return getDayInputAsLines(7)
 }
 
+function cleanContent (item) {
+  return item.slice(0, item.indexOf(' bag')).trim()
+}
+
 function partOne (input) {
-  return input.length
+  const map = {}
+  input.forEach((line) => {
+    const split = line.split(' bags contain ')
+    const [container, rawContents] = [split[0], split[1].split(',').map(cleanContent)]
+
+    rawContents.forEach((content) => {
+      if (content === 'no other') {
+        // ignore
+      } else {
+        const cLessSplit = content.split(' ')
+        const key = `${cLessSplit[1]} ${cLessSplit[2]}`
+        if (map.hasOwnProperty(key)) {
+          map[key].add(container)
+        } else {
+          const set = new Set()
+          set.add(container)
+          map[key] = set
+        }
+      }
+    })
+  })
+  // console.log(map)
+
+  const bagSet = countBags('shiny gold', map, new Set())
+  console.log('END:', bagSet)
+
+  return bagSet.size
+}
+
+function countBags (bagName, map, set) {
+  const bags = map[bagName]
+  if (!bags || bags.size === 0) { return }
+
+  for (const bag of bags.keys()) {
+    set.add(bag)
+    countBags(bag, map, set)
+  }
+  return set
 }
 
 function partTwo (input) {
