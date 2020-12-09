67d1405f88d53d63d2b03db40435a2abe98986cd
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2020-12-09 07:30:36 +0000

    feat(day9|js): part 1 and 2 complete

--- 2020/js/9/main.js
+++ 2020/js/9/main.js
@@ -6,11 +6,58 @@ async function getInput () {
 }
 
 function partOne (input) {
-  return input.length
+  const preambleSize = 25
+  const preamble = input.slice(0, preambleSize)
+
+  let cur
+  for (let i = preambleSize; i < input.length; i++) {
+    cur = input[i]
+    if (!isValid(preamble, cur)) {
+      break
+    }
+    preamble.shift()
+    preamble.push(cur)
+  }
+  return cur
+}
+
+function isValid (arr, val) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (i !== j && Number(arr[i]) + Number(arr[j]) === Number(val)) {
+        return true
+      }
+    }
+  }
+  return false
 }
 
 function partTwo (input) {
-  return input.length
+  const nums = partTwoRunner(input)
+  const max = Math.max(...nums)
+  const min = Math.min(...nums)
+  return max + min
+}
+
+function partTwoRunner (input) {
+  const target = 217430975
+
+  for (let i = 0; i < input.length; i++) {
+    let acc = 0
+    const nums = []
+    for (let j = i; j < input.length; j++) {
+      const num = Number(input[j])
+      acc += num
+      if (acc === target) {
+        return nums
+      } else if (acc > target) {
+        break
+      }
+      nums.push(num)
+    }
+  }
+
+  return []
 }
 
 (async function run () {
@@ -18,14 +65,14 @@ function partTwo (input) {
   console.time('part 1 duration')
   const answerOne = partOne(input)
   console.timeEnd('part 1 duration')
-  const expectedOne = 0
+  const expectedOne = 217430975
   console.log(`part 1 answers. expected: ${expectedOne}, actual: ${answerOne}.`)
   assert.equal(answerOne, expectedOne)
 
   console.time('part 2 duration')
   const answerTwo = partTwo(input)
   console.timeEnd('part 2 duration')
-  const expectedTwo = 0
+  const expectedTwo = 28509180
   console.log(`part 2 answers. expected: ${expectedTwo}, actual: ${answerTwo}.`)
   assert.equal(answerTwo, expectedTwo)
 }())
