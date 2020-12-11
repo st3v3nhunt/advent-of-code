894d980294b2c0aa4ef80f6cfdda15e179890de2
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2020-12-11 09:40:38 +0000

    feat(day11|js): part 1 complete

--- 2020/js/11/main.js
+++ 2020/js/11/main.js
@@ -6,7 +6,63 @@ async function getInput () {
 }
 
 function partOne (input) {
-  return input.length
+  const len = input.length
+  let loop = true
+
+  // initially all seats are filled?!?!?
+  let layout = input.map(line => line.replace(/L/g, '#'))
+  console.log(layout)
+  let count = 1
+  let curSeatCount = 0
+  const rowLen = layout[0].length
+
+  while (loop) {
+    const mutatingLayout = [...layout]
+    for (let i = 0; i < len; i++) {
+      for (let j = 0; j < rowLen; j++) {
+        const chair = layout[i][j]
+
+        if (chair === '.') {
+          continue
+        }
+        // COUNT SEATS
+        const aboveSeats = i > 0 ? layout[i - 1].slice(j > 0 ? j - 1 : 0, j < rowLen - 1 ? j + 2 : rowLen) : ''
+        const belowSeats = i < len - 1 ? layout[i + 1].slice(j > 0 ? j - 1 : 0, j < rowLen - 1 ? j + 2 : rowLen) : ''
+        const leftSeat = j > 0 ? layout[i][j - 1] : ''
+        const rightSeat = j < rowLen ? layout[i][j + 1] : ''
+        const occSeatCount = (`${aboveSeats}${belowSeats}${leftSeat}${rightSeat}`.match(/#/g) ?? []).length
+
+        // MUTATE SEATS
+        const seat = mutatingLayout[i][j]
+        const mutRow = mutatingLayout[i]
+        if (seat === 'L' && occSeatCount === 0) {
+          const arr = mutRow.split('')
+          arr.splice(j, 1, '#')
+          mutatingLayout[i] = arr.join('')
+        } else if (seat === '#' && occSeatCount > 3) {
+          const arr = mutRow.split('')
+          arr.splice(j, 1, 'L')
+          mutatingLayout[i] = arr.join('')
+        }
+      }
+    }
+    const prevSeatCount = curSeatCount
+    curSeatCount = countOccuSeats(mutatingLayout)
+    count++
+    if (curSeatCount === prevSeatCount) {
+      loop = false
+    }
+    layout = [...mutatingLayout]
+  }
+  console.log('iteration: ', count)
+
+  return curSeatCount
+}
+
+function countOccuSeats (arr) {
+  return arr.reduce((acc, cur) => {
+    return acc + (cur.match(/#/g) ?? []).length
+  }, 0)
 }
 
 function partTwo (input) {
@@ -18,7 +74,7 @@ function partTwo (input) {
   console.time('part 1 duration')
   const answerOne = partOne(input)
   console.timeEnd('part 1 duration')
-  const expectedOne = 0
+  const expectedOne = 2164
   console.log(`part 1 answers. expected: ${expectedOne}, actual: ${answerOne}.`)
   assert.equal(answerOne, expectedOne)
 
