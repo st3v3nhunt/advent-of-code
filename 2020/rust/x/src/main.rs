8685a83f360768866d91bc1c6029a584bb0e7b52
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2020-12-12 14:58:43 +0000

    feat(rust): improve template

--- 2020/rust/x/src/main.rs
+++ 2020/rust/x/src/main.rs
@@ -1,19 +1,27 @@
 use std::time::Instant;
 
 fn main() {
-    let file_contents = include_str!("../../../input/0.txt");
+    let file_contents = include_str!("../../../input/x.txt");
 
     let start = Instant::now();
     let ans_one = part_one(file_contents);
     let duration = start.elapsed();
-    println!("Part one answer: {}. Completed in: {:?}", ans_one, duration);
-    assert_eq!(ans_one, 0);
+    let expected_one = 0;
+    println!(
+        "Part one answer: {}, expected {}. Completed in: {:?}",
+        ans_one, expected_one, duration
+    );
+    assert_eq!(ans_one, expected_one);
 
     let start = Instant::now();
     let ans_two = part_two(file_contents);
     let duration = start.elapsed();
-    println!("Part two answer: {}. Completed in: {:?}", ans_two, duration);
-    assert_eq!(ans_two, 0);
+    let expected_two = 0;
+    println!(
+        "Part two answer: {}, expected {}. Completed in: {:?}",
+        ans_two, expected_one, duration
+    );
+    assert_eq!(ans_two, expected_two);
 }
 
 fn part_one(file_contents: &str) -> u32 {
