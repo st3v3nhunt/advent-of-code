c4d9a1424c37bd86b69691983dd83c8d719a8b98
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2020-12-12 08:15:53 +0000

    feat(day12|js): part 1 complete

--- 2020/js/12/main.js
+++ 2020/js/12/main.js
@@ -6,7 +6,67 @@ async function getInput () {
 }
 
 function partOne (input) {
-  return input.length
+  const pos = [0, 0]
+  let deg = 90 // east
+
+  input.forEach(move => {
+    console.log('position and dir', pos, deg)
+    let dir
+    const action = move.slice(0, 1)
+    const val = Number(move.slice(1))
+    console.log(action, val)
+
+    switch (action) {
+      case 'N':
+        pos[1] += val
+        break
+      case 'S':
+        pos[1] -= val
+        break
+      case 'E':
+        pos[0] += val
+        break
+      case 'W':
+        pos[0] -= val
+        break
+      case 'L':
+        deg -= val
+        break
+      case 'R':
+        deg += val
+        break
+      case 'F':
+        dir = deg % 360
+        // if (deg >= 0) {
+        //   dir = deg % 360
+        // } else {
+        //   dir = Math.abs(deg % 360)
+        // }
+        if (dir === 90) {
+          pos[0] += val
+        } else if (dir === -90) {
+          pos[0] -= val
+        } else if (dir === 180) {
+          pos[1] -= val
+        } else if (dir === -180) {
+          pos[1] -= val
+        } else if (dir === 270) {
+          pos[0] -= val
+        } else if (dir === -270) {
+          pos[0] += val
+        } else if (dir === 0) {
+          pos[1] += val
+        } else {
+          Error(`Unknown dir: ${dir}, deg: ${deg}`)
+        }
+        break
+      default:
+        Error(`Unknown instruction ${move}`)
+    }
+  })
+  console.log(pos)
+  const ans = Math.abs(pos[0]) + Math.abs(pos[1])
+  return ans
 }
 
 function partTwo (input) {
