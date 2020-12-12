1e1eb8e4d360492681c159e42a73026645133e29
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2020-12-12 17:29:37 +0000

    feat(day9|rust): part 1 complete

--- 2020/rust/nine/src/main.rs
+++ 2020/rust/nine/src/main.rs
@@ -25,7 +25,34 @@ fn main() {
 }
 
 fn part_one(file_contents: &str) -> i32 {
-    file_contents.len() as i32
+    let preamble_size = 25;
+    let mut preamble = file_contents
+        .lines()
+        .take(preamble_size)
+        .map(|x| x.parse().unwrap())
+        .collect::<Vec<i32>>();
+
+    let mut x: i32 = -1;
+    for line in file_contents.lines().skip(preamble_size) {
+        x = line.parse().unwrap();
+        if !valid(&preamble, x) {
+            break;
+        }
+        preamble.remove(0);
+        preamble.push(x);
+    }
+    x
+}
+
+fn valid(arr: &[i32], x: i32) -> bool {
+    for i in 0..arr.len() {
+        for j in 0..arr.len() {
+            if i != j && arr[i] + arr[j] == x {
+                return true;
+            }
+        }
+    }
+    false
 }
 
 fn part_two(file_contents: &str) -> i32 {
