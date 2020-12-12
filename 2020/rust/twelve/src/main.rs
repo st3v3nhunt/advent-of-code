0d5718298d3c5a1744c6c57d23f2416f33c10fc8
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2020-12-12 16:00:30 +0000

    feat(day12|rust): part 2 complete

--- 2020/rust/twelve/src/main.rs
+++ 2020/rust/twelve/src/main.rs
@@ -19,7 +19,7 @@ fn main() {
     let expected_two = 54404;
     println!(
         "Part two answer: {}, expected {}. Completed in: {:?}",
-        ans_two, expected_one, duration
+        ans_two, expected_two, duration
     );
     assert_eq!(ans_two, expected_two);
 }
@@ -30,7 +30,7 @@ struct Coord {
     y: i32,
 }
 
-fn part_one(file_contents: &str) -> u32 {
+fn part_one(file_contents: &str) -> i32 {
     let mut pos = Coord { x: 0, y: 0 };
     let mut deg = 90;
 
@@ -74,13 +74,74 @@ fn part_one(file_contents: &str) -> u32 {
     });
 
     println!("{:?}", pos);
-    let ans = pos.x.abs() + pos.y.abs();
-    ans as u32
+    pos.x.abs() + pos.y.abs()
 }
 
-fn part_two(file_contents: &str) -> u32 {
-    file_contents.len() as u32
+fn part_two(file_contents: &str) -> i32 {
+    let mut wp = Coord { x: 10, y: 1 };
+    let mut pos = Coord { x: 0, y: 0 };
+    file_contents.lines().for_each(|ins| {
+        let action = ins.chars().next().unwrap();
+        let val = ins
+            .chars()
+            .skip(1)
+            .collect::<String>()
+            .parse::<i32>()
+            .unwrap();
+
+        match action {
+            'N' => wp.y += val,
+            'S' => wp.y -= val,
+            'E' => wp.x += val,
+            'W' => wp.x -= val,
+            'L' => wp = rotate_left(&wp, val),
+            'R' => wp = rotate_right(&wp, val),
+            'F' => {
+                pos.x += wp.x * val;
+                pos.y += wp.y * val;
+            }
+            _ => panic!("Unknown action"),
+        }
+    });
+    pos.x.abs() + pos.y.abs()
 }
 
+fn rotate_left(wp: &Coord, val: i32) -> Coord {
+    let dir = val % 360;
+    match dir {
+        90 | -270 => Coord {
+            x: wp.y * -1,
+            y: wp.x,
+        },
+        -90 | 270 => Coord {
+            x: wp.y,
+            y: wp.x * -1,
+        },
+        180 | -180 => Coord {
+            x: wp.x * -1,
+            y: wp.y * -1,
+        },
+        _ => panic!("Unknown dir for rotating left."),
+    }
+}
+
+fn rotate_right(wp: &Coord, val: i32) -> Coord {
+    let dir = val % 360;
+    match dir {
+        90 | -270 => Coord {
+            x: wp.y,
+            y: wp.x * -1,
+        },
+        -90 | 270 => Coord {
+            x: wp.y * -1,
+            y: wp.x,
+        },
+        180 | -180 => Coord {
+            x: wp.x * -1,
+            y: wp.y * -1,
+        },
+        _ => panic!("Unknown dir for rotating right."),
+    }
+}
 // TODO
 // Use enum for action
