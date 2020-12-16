543bac006cbacce5c231857c761fdb74525fd506
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2020-12-16 23:40:39 +0000

    feat(js|day16): part 2 complete

--- 2020/README.md
+++ 2020/README.md
@@ -28,7 +28,7 @@ language solutions.
 | [13](https://adventofcode.com/2020/day/13) | 171           | JS        | 539746751134958 | JS        |
 | [14](https://adventofcode.com/2020/day/14) | 9879607673316 | JS        |                 |           |
 | [15](https://adventofcode.com/2020/day/15) | 257           | JS, Rust  | 8546398         | JS, Rust  |
-| [16](https://adventofcode.com/2020/day/16) |               |           |                 |           |
+| [16](https://adventofcode.com/2020/day/16) | 23044         | JS        | 3765150732757   | JS        |
 | [17](https://adventofcode.com/2020/day/17) |               |           |                 |           |
 | [18](https://adventofcode.com/2020/day/18) |               |           |                 |           |
 | [19](https://adventofcode.com/2020/day/19) |               |           |                 |           |
--- 2020/js/16/main.js
+++ 2020/js/16/main.js
@@ -17,63 +17,137 @@ async function getInput () {
   console.time('part 2 duration')
   const answerTwo = partTwo(input)
   console.timeEnd('part 2 duration')
-  const expectedTwo = 0
+  const expectedTwo = 3765150732757
   console.log(`part 2 answers. expected: ${expectedTwo}, actual: ${answerTwo}.`)
   assert.equal(answerTwo, expectedTwo)
 }())
 
 function partOne (input) {
   let blanks = 0
-  let skip = true
   const missingNearby = []
-  const nearby = new Set()
-  const valid = new Set()
+  const nearbySet = new Set()
+  const validSet = new Set()
   for (let i = 0; i < input.length; i++) {
     const line = input[i]
     if (line === '') {
       blanks++
     }
     if (blanks === 0) { // valid vals
-      const split = line.split(' ')
+      const split = line.split(': ')
       const candidates = []
-      candidates.push(split[1].split('-').map(Number))
-      candidates.push(split[3].split('-').map(Number))
-      // const [r1, r2] = [split[1].split('-').map(Number), split[3].split('-').map(Number)]
-      // console.log('candidates:', candidates)
-      // console.log(r1, r2)
+      const ranges = split[1].split(' or ')
+      candidates.push(ranges[0].split('-').map(Number))
+      candidates.push(ranges[1].split('-').map(Number))
       candidates.forEach(arr => {
         for (let j = arr[0]; j <= arr[1]; j++) {
-          valid.add(j)
+          validSet.add(j)
         }
       })
-      // const [r1l, r1u, r2l, r2u] = [r1.split('-'), r2.split('-')]
-      console.log(valid)
-    } else if (blanks === 1) { // your ticket
     } else if (blanks === 2) { // nearby tickets
-      // skip on
-      if (skip) {
-        i += 1
-        skip = false
-      }
-      blanks = 4
-    } else if (blanks === 4) { // nearby tickets
+      if (line === '' || line.startsWith('nearby')) { continue }
+
       const tickets = line.split(',').map(Number)
-      console.log('tickets', tickets)
       for (let j = 0; j < tickets.length; j++) {
         const ticket = tickets[j]
-        nearby.add(ticket)
-        if (!valid.has(ticket)) {
-          console.log()
+        nearbySet.add(ticket)
+        if (!validSet.has(ticket)) {
           missingNearby.push(ticket)
         }
       }
     }
   }
-  console.log('number missing:', missingNearby.length)
-  console.log('missing:', missingNearby)
   return missingNearby.reduce((acc, cur) => acc + cur, 0)
 }
 
 function partTwo (input) {
-  return input.length
+  let myTicket = []
+  let blanks = 0
+  const validTickets = []
+  const validSet = new Set()
+  const fields = {}
+  let fieldCount = 0
+
+  for (let i = 0; i < input.length; i++) {
+    const line = input[i]
+    if (line === '') {
+      blanks++
+    }
+    if (blanks === 0) { // valid vals
+      const split = line.split(': ')
+      const candidates = []
+      const field = split[0]
+      const ranges = split[1].split(' or ')
+      candidates.push(ranges[0].split('-').map(Number))
+      candidates.push(ranges[1].split('-').map(Number))
+      const validValues = []
+      candidates.forEach(arr => {
+        for (let j = arr[0]; j <= arr[1]; j++) {
+          validSet.add(j)
+          validValues.push(j)
+        }
+      })
+      fields[field] = validValues
+      fieldCount++
+    } else if (blanks === 1) { // your ticket
+      if (line !== '' && !line.startsWith('your')) {
+        myTicket = line.split(',').map(Number)
+      }
+    } else if (blanks === 2) { // nearby tickets
+      if (line === '' || line.startsWith('nearby')) { continue }
+      const values = line.split(',').map(Number)
+      let invalid = false
+      for (let j = 0; j < values.length; j++) {
+        if (!validSet.has(values[j])) {
+          invalid = true
+        }
+      }
+      if (!invalid) {
+        validTickets.push(values)
+      }
+    }
+  }
+
+  // work out what indexes the fields are not valid at
+  const impossibilities = {}
+  for (const k of Object.keys(fields)) {
+    impossibilities[k] = new Set()
+  }
+  for (let i = 0; i < validTickets.length; i++) {
+    for (const field of Object.keys(fields)) {
+      for (let j = 0; j < fieldCount; j++) {
+        // check if the field val is within the bounds for the field
+        if (fields[field].indexOf(validTickets[i][j]) < 0) {
+          // field not in bounds, field can not be in at this position
+          impossibilities[field].add(j)
+        }
+      }
+    }
+  }
+
+  // work out the order of the fields
+  const possibleFieldIndexes = [...Array(fieldCount).keys()]
+  const fieldOrder = {}
+  for (let i = 0; i < fieldCount; i++) {
+    Object.entries(impossibilities).forEach(([k, v]) => {
+      // -1 due to one field being possible in all indexes
+      if (v.size === possibleFieldIndexes.length - 1) {
+        for (let j = 0; j < possibleFieldIndexes.length; j++) {
+          const val = possibleFieldIndexes[j]
+          if (!v.has(val)) {
+            fieldOrder[k] = val
+            possibleFieldIndexes.splice(j, 1)
+          }
+        }
+      }
+    })
+  }
+
+  let ans = 1
+  for (const [k, v] of Object.entries(fieldOrder)) {
+    if (k.startsWith('departure')) {
+      ans *= myTicket[v]
+    }
+  }
+
+  return ans
 }
