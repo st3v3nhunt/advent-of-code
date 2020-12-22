c752d219205d2b685fa8822ba445d9d7539280b0
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2020-12-22 10:24:16 +0000

    feat(js|day22): part 2 complete

--- 2020/js/22/main.js
+++ 2020/js/22/main.js
@@ -7,17 +7,17 @@ async function getInput () {
 
 (async function run () {
   const input = await getInput()
-  // console.time('part 1 duration')
-  // const answerOne = partOne(input)
-  // console.timeEnd('part 1 duration')
-  // const expectedOne = 32401
-  // console.log(`part 1 answers. expected: ${expectedOne}, actual: ${answerOne}.`)
-  // assert.equal(answerOne, expectedOne)
+  console.time('part 1 duration')
+  const answerOne = partOne(input)
+  console.timeEnd('part 1 duration')
+  const expectedOne = 32401
+  console.log(`part 1 answers. expected: ${expectedOne}, actual: ${answerOne}.`)
+  assert.equal(answerOne, expectedOne)
 
   console.time('part 2 duration')
   const answerTwo = partTwo(input)
   console.timeEnd('part 2 duration')
-  const expectedTwo = 0
+  const expectedTwo = 31436
   console.log(`part 2 answers. expected: ${expectedTwo}, actual: ${answerTwo}.`)
   assert.equal(answerTwo, expectedTwo)
 }())
@@ -29,10 +29,8 @@ function processInput (input) {
 
   for (let i = 0; i < input.length; i++) {
     const line = input[i]
-    console.log('line:', line)
     if (line.startsWith('Player')) {
       playerNum = Number(line.split(' ')[1][0])
-      console.log(playerNum)
     } else if (line === '') {
       continue
     } else {
@@ -48,40 +46,73 @@ function processInput (input) {
 
 function partOne (input) {
   const [p1, p2] = processInput(input)
-  console.log('player1:', p1)
-  console.log('player2:', p2)
-  let round = 0
   while (p1.length > 0 && p2.length > 0) {
     const c1 = p1.shift()
     const c2 = p2.shift()
     if (c1 > c2) {
       p1.push(c1)
       p1.push(c2)
-    } else { // there aren't any 2 cards of the same value
+    } else {
       p2.push(c2)
       p2.push(c1)
     }
-
-    round++
   }
-  // console.log('number of rounds:', round)
-  // console.log('player1 deck:', p1)
-  // console.log('player2 deck:', p2)
   const p1Score = calcWinningScore(p1)
   const p2Score = calcWinningScore(p2)
-  // console.log('player1 score:', p1Score)
-  // console.log('player2 score:', p2Score)
   return p1Score > p2Score ? p1Score : p2Score
 }
 
 function calcWinningScore (deck) {
   const len = deck.length
   return deck.reduce((acc, cur, i) => {
-    // console.log(acc, cur, len - i)
     return acc + (cur * (len - i))
   }, 0)
 }
 
+function recursiveCombat (p1, p2) {
+  const states = new Set()
+  // draw
+  while (p1.length > 0 && p2.length > 0) {
+    const state = p1.join(',') + '-' + p2.join(',')
+    if (states.has(state)) {
+      return [p1, p2, 'p1']
+    }
+    const c1 = p1.shift()
+    const c2 = p2.shift()
+    states.add(state)
+    let roundWinner
+
+    if (p1.length >= c1 && p2.length >= c2) {
+      const [d1, d2, winner] = recursiveCombat(p1.slice(0, c1), p2.slice(0, c2))
+      if (winner) {
+        // an instant end gives the win to p1
+        roundWinner = winner
+      } else {
+        // highest value card in deck wins
+        const d1high = d1.reduce((acc, cur) => cur > acc ? cur : acc, 0)
+        const d2high = d2.reduce((acc, cur) => cur > acc ? cur : acc, 0)
+        roundWinner = d1high > d2high ? 'p1' : 'p2'
+      }
+    } else {
+      // highest drawn card wins
+      roundWinner = c1 > c2 ? 'p1' : 'p2'
+    }
+    if (roundWinner === 'p1') {
+      p1.push(c1)
+      p1.push(c2)
+    } else {
+      p2.push(c2)
+      p2.push(c1)
+    }
+  }
+  return [p1, p2]
+}
+
 function partTwo (input) {
-  return input.length
+  const [p1, p2] = processInput(input)
+  const [d1, d2] = recursiveCombat(p1, p2)
+
+  const p1Score = calcWinningScore(d1)
+  const p2Score = calcWinningScore(d2)
+  return p1Score > p2Score ? p1Score : p2Score
 }
