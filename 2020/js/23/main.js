74f2c7f8703ed8463c9478e85e043410eadc2c38
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2020-12-23 08:41:59 +0000

    brute force approach

--- 2020/js/23/main.js
+++ 2020/js/23/main.js
@@ -4,7 +4,7 @@ function run () {
   console.time('part 1 duration')
   const answerOne = partOne()
   console.timeEnd('part 1 duration')
-  const expectedOne = 0
+  const expectedOne = 26354798
   console.log(`part 1 answers. expected: ${expectedOne}, actual: ${answerOne}.`)
   assert.equal(answerOne, expectedOne)
 
@@ -27,7 +27,6 @@ function partOne () {
   const takeSize = 3
   const lowestCup = cups.reduce((acc, cur) => acc < cur ? acc : cur, Number(input))
   const highestCup = cups.reduce((acc, cur) => acc > cur ? acc : cur, 0)
-  console.log(lowestCup, '************', highestCup)
 
   while (i < moves) {
     console.log('move', i + 1, 'cups', cups.join(','), 'cur', cur)
@@ -41,12 +40,11 @@ function partOne () {
       takenCups.push(...cups.splice(ccIndex + 1, availableCupCount))
       takenCups.push(...cups.splice(0, takeSize - availableCupCount))
     }
-    console.log('taken', takenCups.join(','))
+    // console.log('taken', takenCups.join(','))
 
     // add to destination
     let dest = cur - 1
     let destFound = false
-    console.log('dest', dest, 'low', lowestCup)
     while (!destFound) {
       if (dest < lowestCup) {
         dest = highestCup
@@ -54,14 +52,12 @@ function partOne () {
       if (takenCups.includes(dest)) {
         dest--
       } else {
-        console.log('dest found')
         destFound = true
       }
     }
 
     // add cups
     const dcIndex = cups.indexOf(dest)
-    console.log('dcIndex', dcIndex, 'dest', dest)
     cups.splice(dcIndex + 1, 0, ...takenCups)
 
     // new cur
@@ -79,14 +75,79 @@ function partOne () {
   const finalCups = cups.splice(oneIndex + 1)
   // remove 1
   cups.pop()
-  console.log('cups', cups)
   const final = finalCups.concat(...cups)
-  console.log('finalCups', final)
 
-  const ans = final.join('')
+  return final.join('')
+}
+
+function partTwo () {
+  // const input = '284573961'
+  const input = '389125467'
+  const initialCups = input.split('').map(Number)
+  const cups = genAllCups(initialCups)
+  let cur = initialCups[0]
+  const moves = 10000000
+  const cupCount = cups.length
+  let i = 0
+  const takeSize = 3
+  const lowestCup = 1
+  const highestCup = 1000000
+
+  while (i < moves) {
+    // console.log('move', i + 1, 'cups', cups.join(','), 'cur', cur)
+    // take cups
+    const takenCups = []
+    let ccIndex = cups.indexOf(cur)
+    if (ccIndex + takeSize < cupCount) { // enough cups available for a take)
+      takenCups.push(...cups.splice(ccIndex + 1, 3))
+    } else { // need to wrap to start
+      const availableCupCount = (cupCount - 1) - ccIndex
+      takenCups.push(...cups.splice(ccIndex + 1, availableCupCount))
+      takenCups.push(...cups.splice(0, takeSize - availableCupCount))
+    }
+    // console.log('taken', takenCups.join(','))
+
+    // add to destination
+    let dest = cur - 1
+    let destFound = false
+    while (!destFound) {
+      if (dest < lowestCup) {
+        dest = highestCup
+      }
+      if (takenCups.includes(dest)) {
+        dest--
+      } else {
+        destFound = true
+      }
+    }
+
+    // add cups
+    const dcIndex = cups.indexOf(dest)
+    cups.splice(dcIndex + 1, 0, ...takenCups)
+
+    // new cur
+    ccIndex = cups.indexOf(cur)
+    if (ccIndex + 1 === cupCount) { // cur at end
+      cur = cups[0]
+    } else {
+      cur = cups[ccIndex + 1]
+    }
+    i++
+  }
+
+  // collect cups
+  const oneIndex = cups.indexOf(1)
+  const finalCups = cups.splice(oneIndex + 1, 2)
+  console.log(finalCups)
+  const ans = finalCups[0] * finalCups[1]
+
   return ans
 }
 
-function partTwo (input) {
-  return input.length
+function genAllCups (initialCups) {
+  const cups = [...Array(1000001).keys()]
+  console.log(cups.shift())
+  cups.splice(0, initialCups.length, ...initialCups)
+  console.log(cups)
+  return cups
 }
