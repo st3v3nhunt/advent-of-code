60495e08905bdd854fee82a545b3d8124d708282
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2020-12-23 16:28:05 +0000

    Day 23 (#6)
    * brute force approach
    
    * refactor(day23|js): use map rathr than array
    
    * feat(day23|js): part 2 complete

--- 2020/js/23/main.js
+++ 2020/js/23/main.js
@@ -4,14 +4,14 @@ function run () {
   console.time('part 1 duration')
   const answerOne = partOne()
   console.timeEnd('part 1 duration')
-  const expectedOne = 0
+  const expectedOne = '26354798'
   console.log(`part 1 answers. expected: ${expectedOne}, actual: ${answerOne}.`)
   assert.equal(answerOne, expectedOne)
 
   console.time('part 2 duration')
   const answerTwo = partTwo()
   console.timeEnd('part 2 duration')
-  const expectedTwo = 0
+  const expectedTwo = '166298218695'
   console.log(`part 2 answers. expected: ${expectedTwo}, actual: ${answerTwo}.`)
   assert.equal(answerTwo, expectedTwo)
 }
@@ -19,34 +19,50 @@ run()
 
 function partOne () {
   const input = '284573961'
-  const cups = input.split('').map(Number)
-  let cur = cups[0]
+  const cur = Number(input[0])
+
+  const startCups = new Map()
+  for (let i = 0; i < input.length; i++) {
+    const val = Number(input[i])
+    const next = i === input.length - 1 ? input[0] : input[i + 1]
+    startCups.set(val, Number(next))
+  }
   const moves = 100
-  const cupCount = cups.length
-  let i = 0
+
+  const endCups = play(startCups, moves, cur)
+
+  // collect cups
+  let acc = endCups.get(1)
+  const order = []
+  while (acc !== 1) {
+    const next = acc
+    order.push(next)
+    acc = endCups.get(next)
+  }
+  return order.join('')
+}
+
+function play (cups, moves, cur) {
   const takeSize = 3
-  const lowestCup = cups.reduce((acc, cur) => acc < cur ? acc : cur, Number(input))
-  const highestCup = cups.reduce((acc, cur) => acc > cur ? acc : cur, 0)
-  console.log(lowestCup, '************', highestCup)
+  const lowestCup = 1
+  const highestCup = cups.size
+  let i = 0
 
   while (i < moves) {
-    console.log('move', i + 1, 'cups', cups.join(','), 'cur', cur)
     // take cups
     const takenCups = []
-    let ccIndex = cups.indexOf(cur)
-    if (ccIndex + takeSize < cupCount) { // enough cups available for a take)
-      takenCups.push(...cups.splice(ccIndex + 1, 3))
-    } else { // need to wrap to start
-      const availableCupCount = (cupCount - 1) - ccIndex
-      takenCups.push(...cups.splice(ccIndex + 1, availableCupCount))
-      takenCups.push(...cups.splice(0, takeSize - availableCupCount))
+    let next = cur
+    for (let i = 0; i < takeSize; i++) {
+      const nextTemp = cups.get(next)
+      takenCups.push(nextTemp)
+      next = nextTemp
     }
-    console.log('taken', takenCups.join(','))
+    // update cur to point to next available i.e. after taken cups
+    cups.set(cur, cups.get(takenCups[takeSize - 1]))
 
     // add to destination
     let dest = cur - 1
     let destFound = false
-    console.log('dest', dest, 'low', lowestCup)
     while (!destFound) {
       if (dest < lowestCup) {
         dest = highestCup
@@ -54,39 +70,53 @@ function partOne () {
       if (takenCups.includes(dest)) {
         dest--
       } else {
-        console.log('dest found')
         destFound = true
       }
     }
 
-    // add cups
-    const dcIndex = cups.indexOf(dest)
-    console.log('dcIndex', dcIndex, 'dest', dest)
-    cups.splice(dcIndex + 1, 0, ...takenCups)
+    // place cups
+    const destNext = cups.get(dest)
+    cups.set(takenCups[takeSize - 1], destNext)
+    cups.set(dest, takenCups[0])
 
     // new cur
-    ccIndex = cups.indexOf(cur)
-    if (ccIndex + 1 === cupCount) { // cur at end
-      cur = cups[0]
-    } else {
-      cur = cups[ccIndex + 1]
-    }
+    cur = cups.get(cur)
+
     i++
   }
-
-  // collect cups
-  const oneIndex = cups.indexOf(1)
-  const finalCups = cups.splice(oneIndex + 1)
-  // remove 1
-  cups.pop()
-  console.log('cups', cups)
-  const final = finalCups.concat(...cups)
-  console.log('finalCups', final)
-
-  const ans = final.join('')
-  return ans
+  return cups
 }
 
-function partTwo (input) {
-  return input.length
+function partTwo () {
+  const input = '284573961'
+  const cur = Number(input[0])
+
+  const startCups = genAllCups(input)
+  const moves = 10000000
+
+  const endCups = play(startCups, moves, cur)
+
+  // collect cups
+  const first = endCups.get(1)
+  const second = endCups.get(first)
+  console.log('cup 1', first)
+  console.log('cup 2', second)
+
+  return first * second
+}
+
+function genAllCups (input) {
+  const cSize = 1000000
+  const cups = new Map()
+  const head = Number(input[0])
+  for (let i = 0; i < input.length; i++) {
+    const val = Number(input[i])
+    const next = i === input.length - 1 ? input.length + 1 : input[i + 1]
+    cups.set(val, Number(next))
+  }
+  for (let i = input.length + 1; i <= cSize; i++) {
+    const next = i === cSize ? head : i + 1
+    cups.set(i, next)
+  }
+  return cups
 }
