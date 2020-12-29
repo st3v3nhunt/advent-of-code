257d9b8f293f8dfa6f02ed2b821ec0d79d34bfbd
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2020-12-29 18:07:32 +0000

    wip(day20|js): part two

--- 2020/js/20/main.js
+++ 2020/js/20/main.js
@@ -7,12 +7,12 @@ async function getInput () {
 
 (async function run () {
   const input = await getInput()
-  console.time('part 1 duration')
-  const answerOne = partOne(input)
-  console.timeEnd('part 1 duration')
-  const expectedOne = 17148689442341
-  console.log(`part 1 answers. expected: ${expectedOne}, actual: ${answerOne}.`)
-  assert.equal(answerOne, expectedOne)
+  // console.time('part 1 duration')
+  // const answerOne = partOne(input)
+  // console.timeEnd('part 1 duration')
+  // const expectedOne = 17148689442341
+  // console.log(`part 1 answers. expected: ${expectedOne}, actual: ${answerOne}.`)
+  // assert.equal(answerOne, expectedOne)
 
   console.time('part 2 duration')
   const answerTwo = partTwo(input)
@@ -23,20 +23,148 @@ async function getInput () {
 }())
 
 function partOne (input) {
-  const tilesAndEdges = processInputIntoTiles(input)
-  const corners = getCorners(tilesAndEdges)
-  return corners.reduce((acc, cur) => acc * cur, 1)
-}
-
-function partTwo (input) {
-  return input.length
-}
-
-function processInputIntoTiles (input) {
   const inputTiles = transformInputToTiles(input)
   const tileTranslations = createTranslations(inputTiles)
   const borders = getBorders(tileTranslations)
-  return getTilesAndEdges(borders)
+  const tilesAndEdges = getTilesAndEdges(borders)
+  const corners = getCorners(tilesAndEdges)
+  return [...corners.values()].reduce((acc, cur) => acc * cur, 1)
+}
+
+function partTwo (input) {
+  const inputTiles = transformInputToTiles(input)
+  const tileTranslations = createTranslations(inputTiles)
+  const borders = getBorders(tileTranslations)
+  const tilesAndEdges = getTilesAndEdges(borders)
+  const corners = getCorners(tilesAndEdges)
+  const edges = getEdges(tilesAndEdges)
+  const centers = getCenters(tilesAndEdges)
+
+  console.log('tilesAndEdges:', tilesAndEdges)
+  console.log('corners:', corners)
+  console.log('edges:', edges)
+  console.log('centers:', centers)
+
+  // const row1 = []
+  // const row2 = []
+  // start with a corner
+  // based on the grid size there are:
+  // - 4 corners (always)
+  // - (gridSize - 2) edges
+  // create the first row based on any corner
+  const gridSize = Math.sqrt(inputTiles.size)
+  const grid = [...new Array(gridSize)].map(x => [])
+  const edgeCount = gridSize - 2
+
+  // take a corner and look up the connectors it has
+  // then for as many edges tiles as there are, take one
+  // when at the other corner, do the same
+  let c1 = corners.values().next().value // get corner - 1951
+  const c1cs = tilesAndEdges.get(c1) // find conns - 2311, 2729
+  const e1 = c1cs.values().next().value // take a conn - 2311
+  c1cs.delete(e1) // remove conn from corner
+  const e1cs = tilesAndEdges.get(e1) // get conns of edge - 1427, 1951, 3079
+  e1cs.delete(c1) // delete current c1 from list, new list - 1427, 3079
+  let cur
+  for (const val of e1cs.values()) { // check if any of them are edges
+    // if (col !== gridSize) { // if not at edge of grid
+    let edgeFound = false
+    for (const edge of edges.values()) {
+      if (edge === val) {
+        cur = tilesAndEdges.get(val) // next tile to lay - 3079
+        cur.delete(e1)
+        edgeFound = true
+        corners.delete(val)
+        break
+      }
+    }
+    if (!edgeFound) { // must be a corner!??!
+      for (const edge of corners.values()) {
+        if (edge === val) {
+          cur = tilesAndEdges.get(val) // next tile to lay - 3079
+          cur.delete(e1)
+          corners.delete(val)
+          break
+        }
+      }
+    }
+  }
+
+  grid[0].push(c1)
+  let row = 0
+  while (true) {
+    for (let i = 0; i < edgeCount; i++) { // + 1 for the last corner
+      const c1conns = tilesAndEdges.get(c1)
+      for (const e1 of c1conns.values()) {
+        if (edges.delete(e1)) { // got edge
+          c1conns.delete(e1)
+          grid[row].push(e1)
+          c1 = e1
+          break
+        }
+      }
+    }
+    // find next corner
+    const c2 = tilesAndEdges.get(c1)
+    for (const corner of c2.values()) {
+      if (corners.delete(corner)) {
+        c2.delete(corner)
+        grid[row].push(corner)
+        c1 = corner
+        break
+      }
+    }
+    row++
+  }
+
+  while (true) {
+    const ref = tilesAndEdges.get(c1)
+    grid[0].push(c1)
+    for (const tile of ref.values()) {
+      if (edges.delete(tile)) { // got edge
+        grid[0].push(tile) // TODO: change the index of the grid!
+        ref.delete(tile) // remove tile ref
+        c1 = tile
+        break
+      } else if (corners.delete(tile)) { // got corner
+        grid[0].push(tile) // TODO: change the index of the grid!
+        ref.delete(tile) // remove tile ref
+        c1 = tile
+        break
+      }
+    }
+  }
+
+  corners.delete(c1)
+  // can now add the 2 this connects too
+  const c1edges = tilesAndEdges.get(c1).values()
+  console.log(c1edges)
+  const nextEdge = c1edges.next().value
+  // get next in row, look in edges
+  grid[0].push(nextEdge)
+  if (edges.delete(nextEdge)) { // if this is an edge, add it as such
+    // moving along the row
+  } else if (corners.delete(nextEdge)) {
+    // at the corner, need to move to next row
+  }
+  tilesAndEdges.get(nextEdge)
+
+  const nextCol = c1edges.next().value
+  grid[1].push(nextCol)
+  // c1 is done now - could remove it
+
+  console.log(grid)
+  grid.forEach(x => console.log(x.join(',')))
+  // start with a corner i.e. a key with only 2 values
+  // work across
+  //
+  // figure out tile order, must be the specific translation
+  // remove borders from tiles
+  // stitch together
+  // create translations
+  // search for monsters
+  // calculate the roughness
+  return input.length
 }
 
 function transformInputToTiles (input) {
@@ -61,7 +189,7 @@ function transformInputToTiles (input) {
 }
 
 function getTilesAndEdges (borders) {
-  const corners = new Map()
+  const tiles = new Map()
   borders.forEach((tileIdsSet, border) => {
     const tileIds = [...tileIdsSet]
     const len = tileIds.length
@@ -69,16 +197,15 @@ function getTilesAndEdges (borders) {
       for (let i = 0; i < len; i++) {
         const k = tileIds[i]
         const val = tileIds[1 - i]
-        if (corners.has(k)) {
-          corners.get(k).add(val)
+        if (tiles.has(k)) {
+          tiles.get(k).add(val)
         } else {
-          corners.set(k, new Set([val]))
+          tiles.set(k, new Set([val]))
         }
       }
     }
   })
-  console.log('tiles in image:', corners.size)
-  return corners
+  return tiles
 }
 
 // returns a map of all borders (as the key) and the tile(s) each border belongs too
@@ -136,14 +263,26 @@ function createTranslations (tiles) {
   return tileTranslations
 }
 
-function getCorners (tilesAndEdges) {
-  const cornerTileIds = []
-  tilesAndEdges.forEach((v, k) => {
-    if (v.size === 2) {
-      cornerTileIds.push(k)
+function getCorners (tiles) {
+  return getTilesWithConnectedEdges(tiles, 2)
+}
+
+function getEdges (tiles) {
+  return getTilesWithConnectedEdges(tiles, 3)
+}
+
+function getCenters (tiles) {
+  return getTilesWithConnectedEdges(tiles, 4)
+}
+
+function getTilesWithConnectedEdges (tiles, connections) {
+  const tileIds = new Set()
+  tiles.forEach((v, k) => {
+    if (v.size === connections) {
+      tileIds.add(k)
     }
   })
-  return cornerTileIds
+  return tileIds
 }
 
 // returns a new array with the contents translated by 90 degrees right
