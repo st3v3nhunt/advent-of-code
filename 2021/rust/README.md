62807a08f654dff1378c37dbb0dc54fb1fa1c1cf
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2021-11-29 09:41:00 +0000

    feat: set Rust for 2021

---
+++ 2021/rust/README.md
@@ -0,0 +1,18 @@
+# Rust solution attempts
+
+## Setup
+
+Each day is its own crate. Because a crate can not have a name starting with
+the numeric character each day is spelt e.g. day 1 is `one`, day 20 is
+`twenty`.
+
+Make a copy of the `x` directory and rename it for the day e.g. from the `rust`
+directory run `cp -r x one`, for day `one`. In the new directory update the
+package name in `Cargo.toml` to match the name of the day.
+
+### Running
+
+[cargo-watch](https://crates.io/crates/cargo-watch) is used to help with
+running the solutions. See the link for additional information on installing
+and detailed usage. Basic use is achieved with `cargo watch -x run` in the
+directory of the day being worked on.
---
+++ 2021/rust/x/src/main.rs
@@ -0,0 +1,36 @@
+use std::time::Instant;
+
+fn main() {
+    // TODO: Update the location of the input file.
+    let file_contents = include_str!("../../../input/x.txt");
+
+    let start = Instant::now();
+    let ans_one = part_one(file_contents);
+    let duration = start.elapsed();
+    // TODO: Update the expected answer (once known).
+    let expected_one = 0;
+    println!(
+        "Part one answer: {}, expected {}. Completed in: {:?}",
+        ans_one, expected_one, duration
+    );
+    assert_eq!(ans_one, expected_one);
+
+    let start = Instant::now();
+    let ans_two = part_two(file_contents);
+    let duration = start.elapsed();
+    // TODO: Update the expected answer (once known).
+    let expected_two = 0;
+    println!(
+        "Part two answer: {}, expected {}. Completed in: {:?}",
+        ans_two, expected_two, duration
+    );
+    assert_eq!(ans_two, expected_two);
+}
+
+fn part_one(file_contents: &str) -> i32 {
+    file_contents.len() as i32
+}
+
+fn part_two(file_contents: &str) -> i32 {
+    file_contents.len() as i32
+}
