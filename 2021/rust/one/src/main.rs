81c905dbad6a047b42e4837915f5d8652d318e0a
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2021-12-01 08:14:15 +0000

    feat(rust): day one complete

--- 2021/rust/one/src/main.rs
+++ 2021/rust/one/src/main.rs
@@ -1,13 +1,12 @@
 use std::time::Instant;
 
 fn main() {
-    let file_contents = include_str!("../../../input/one.txt");
+    let lines = include_str!("../../../input/one.txt").lines();
 
     let start = Instant::now();
-    let ans_one = part_one(file_contents);
+    let ans_one = part_one(lines.clone());
     let duration = start.elapsed();
-    // TODO: Update the expected answer (once known).
-    let expected_one = 0;
+    let expected_one = 1316;
     println!(
         "Part one answer: {}, expected {}. Completed in: {:?}",
         ans_one, expected_one, duration
@@ -15,10 +14,9 @@ fn main() {
     assert_eq!(ans_one, expected_one);
 
     let start = Instant::now();
-    let ans_two = part_two(file_contents);
+    let ans_two = part_two(lines);
     let duration = start.elapsed();
-    // TODO: Update the expected answer (once known).
-    let expected_two = 0;
+    let expected_two = 1344;
     println!(
         "Part two answer: {}, expected {}. Completed in: {:?}",
         ans_two, expected_two, duration
@@ -26,10 +24,36 @@ fn main() {
     assert_eq!(ans_two, expected_two);
 }
 
-fn part_one(file_contents: &str) -> i32 {
-    file_contents.len() as i32
+fn part_one(lines: std::str::Lines) -> i32 {
+    let mut increment = 0;
+    let numbers = lines
+        .map(|x| x.parse::<i32>().unwrap())
+        .collect::<Vec<i32>>();
+
+    let mut i = 1;
+    while i < numbers.len() {
+        if numbers[i] > numbers[i - 1] {
+            increment = increment + 1;
+        }
+        i = i + 1;
+    }
+    increment
 }
 
-fn part_two(file_contents: &str) -> i32 {
-    file_contents.len() as i32
+fn part_two(lines: std::str::Lines) -> i32 {
+    let mut increment = 0;
+    let numbers = lines
+        .map(|x| x.parse::<i32>().unwrap())
+        .collect::<Vec<i32>>();
+
+    let mut i = 3;
+    while i < numbers.len() {
+        let prev = numbers[i - 1] + numbers[i - 2] + numbers[i - 3];
+        let curr = numbers[i] + numbers[i - 1] + numbers[i - 2];
+        if curr > prev {
+            increment = increment + 1;
+        }
+        i = i + 1;
+    }
+    increment
 }
