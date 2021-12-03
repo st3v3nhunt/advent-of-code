1d7f99a4e2482fb1ede7d6f40484d0c363032e9c
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2021-12-03 12:58:31 +0000

    refactor(rust): reduce duplication

--- 2021/rust/x/src/main.rs
+++ 2021/rust/x/src/main.rs
@@ -1,30 +1,35 @@
 use std::time::Instant;
 
-fn main() {
+fn solve_part(solver: &dyn Fn(std::str::Lines) -> i32, expected: i32) {
     // TODO: Update the location of the input file.
-    let lines = include_str!("../../../input/x.txt").lines();
+    let lines = include_str!("../../../input/three.txt").lines();
+    run_part(lines, solver, expected)
+}
 
+fn test_part(solver: &dyn Fn(std::str::Lines) -> i32, expected: i32) {
+    // TODO: Update the location of the input file.
+    let lines = include_str!("../../../input/test/three.txt").lines();
+    run_part(lines, solver, expected)
+}
+
+fn run_part(lines: std::str::Lines, solver: &dyn Fn(std::str::Lines) -> i32, expected: i32) {
     let start = Instant::now();
-    let ans_one = part_one(lines.clone());
+    let answer = solver(lines.clone());
     let duration = start.elapsed();
-    // TODO: Update the expected answer (once known).
-    let expected_one = 0;
     println!(
         "Part one answer: {}, expected {}. Completed in: {:?}",
-        ans_one, expected_one, duration
+        answer, expected, duration
     );
-    assert_eq!(ans_one, expected_one);
+    assert_eq!(answer, expected);
+}
 
-    let start = Instant::now();
-    let ans_two = part_two(lines);
-    let duration = start.elapsed();
-    // TODO: Update the expected answer (once known).
-    let expected_two = 0;
-    println!(
-        "Part two answer: {}, expected {}. Completed in: {:?}",
-        ans_two, expected_two, duration
-    );
-    assert_eq!(ans_two, expected_two);
+fn main() {
+    // TODO: Update expected answers
+    test_part(&part_one, 1);
+    solve_part(&part_one, 1);
+
+    test_part(&part_two, 2);
+    solve_part(&part_two, 2);
 }
 
 fn part_one(lines: std::str::Lines) -> i32 {
