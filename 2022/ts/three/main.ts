3ef8bc588029edc3f4a2380020a8fa6585ea1487
Steve Hunt <st3v3nhunt@users.noreply.github.com>
2022-12-03 17:40:25 +0000

    feat: day three 2022

--- 2022/ts/three/main.ts
+++ 2022/ts/three/main.ts
@@ -2,18 +2,57 @@ import { solve, test } from "../lib/runner.ts";
 
 async function run() {
   const day = "three";
-  await test(day, 1, partOne, 0);
-  await solve(day, 1, partOne, 0);
-  await test(day, 2, partTwo, 0);
-  await solve(day, 2, partTwo, 0);
+  await test(day, 1, partOne, 157);
+  await solve(day, 1, partOne, 8202);
+  await test(day, 2, partTwo, 70);
+  await solve(day, 2, partTwo, 2864);
+}
+
+function calcPriority(char: string) {
+  const code = char.charCodeAt(0) - 96;
+  return code < 0 ? code + 58 : code
 }
 
 function partOne(input: Array<string>): number {
-  return input.length;
+  let prioritySum = 0;
+  input.forEach((x) => {
+    const mid = x.length / 2;
+    const first = x.slice(0, mid);
+    const second = x.slice(mid);
+    let sharedItem: string;
+    first.split("").forEach((x) => {
+      if (second.includes(x)) {
+        sharedItem = x;
+      }
+    });
+    const priority = calcPriority(sharedItem);
+    prioritySum += priority;
+  });
+  return prioritySum;
 }
 
 function partTwo(input: Array<string>): number {
-  return input.length;
+  let prioritySum = 0;
+  for (let i = 0; i < input.length; i += 3) {
+    const one = input[i];
+    const two = input[i + 1];
+    const three = input[i + 2];
+    const sharedItems: Array<string> = [];
+    one.split("").forEach((x) => {
+      if (two.includes(x)) {
+        sharedItems.push(x);
+      }
+    });
+    let sharedItem: string;
+    sharedItems.forEach((x) => {
+      if (three.includes(x)) {
+        sharedItem = x;
+      }
+    });
+    const priority = calcPriority(sharedItem);
+    prioritySum += priority;
+  }
+  return prioritySum;
 }
 
 await run();
